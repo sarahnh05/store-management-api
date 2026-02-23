446c51667f8778d55c99fb39357b1150384efb73
sarahnh05 <sarahnurhaibah@gmail.com>
2026-02-23 16:36:42 +0700

    add userRoutes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
-const express = require('express');
+import express from 'express';
+
+//import routes
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
+
+// API routes
+app.use('/users', userRoutes);
 
 const PORT = 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
+
+// http://localhost:3000/
