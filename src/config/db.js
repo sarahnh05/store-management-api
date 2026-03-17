e255d0a65fc6b4ba454654e0307889c643af2895
sarahnh05 <sarahnurhaibah@gmail.com>
2026-03-18 04:24:41 +0700

    model product

---
+++ src/config/db.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client/extension';
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === 'development'
+      ? ['query', 'error', 'warn']
+      : ['error'],
+});
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log('DB connected via Prisma');
+  } catch (error) {
+    console.log(`Database connection error: ${error.message}
+    `);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+
+export { prisma, connectDB, disconnectDB };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { config } from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
 
 //import routes
 import userRoutes from './routes/userRoutes.js';
+
+config();
+connectDB();
 
 const app = express();
 
@@ -11,6 +16,33 @@ app.use('/users', userRoutes);
 const PORT = 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
+});
+
+// error handling db
+
+// handle unhandled promise rejections (ex: db connection error)
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// handle uncaught exceptions
+process.on('uncaughtException', async (err) => {
+  console.error('Uncaught Exception:', err);
+  await disconnectDB();
+  process.exit(1);
+});
+
+// Graceful shutdown
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM received, shutting down gracefully');
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
 });
 
 // http://localhost:3000/
